1a0d0f7a150e5cdd1f4674c2f00d2bd79e0b9884
MaksymVorozhtsov <maxim.vorojtsov@gmail.com>
2018-11-14 19:47:38 +0100

    travis pt 2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 "use strict";
 
-let screens = ["welcome", "game-genre", "game-artist", "result-success", "fail-time", "fail-tries", "modal-error", "modal-confirm"];
+let screens = [`welcome`, `game-genre`, `game-artist`, `result-success`, `fail-time`, `fail-tries`, `modal-error`, `modal-confirm`];
 
-const mainSection = document.querySelector(".main");
-const arrowsSection = document.querySelector(".arrows");
+const mainSection = document.querySelector(`.main`);
+const arrowsSection = document.querySelector(`.arrows`);
 
 const arrowShow = function () {
-  let arrowsToInsert = document.getElementById("arrows").content;
+  let arrowsToInsert = document.getElementById(`arrows`).content;
   arrowsSection.appendChild(arrowsToInsert);
 };
 
@@ -14,8 +14,8 @@ arrowShow();
 
 const RIGHT_KEYCODE = 39;
 const LEFT_KEYCODE = 37;
-const RIGHT_BTN = document.querySelector(".arrows__btn--right");
-const LEFT_BTN = document.querySelector(".arrows__btn--left");
+const RIGHT_BTN = document.querySelector(`.arrows__btn--right`);
+const LEFT_BTN = document.querySelector(`.arrows__btn--left`);
 
 const screenShow = function (id) {
   let templateToInsert = document.getElementById(id).content;
@@ -32,7 +32,7 @@ let i = 0;
 const swichRight = function () {
   i++;
   if (i > 0 && i <= screens.length - 1) {
-    mainSection.innerHTML = "";
+    mainSection.innerHTML = ``;
     screenShow(screens[i]);
   } else {
     i = screens.length - 1;
@@ -42,14 +42,14 @@ const swichRight = function () {
 const swichLeft = function() {
   i--;
   if (i >= 0 && i < screens.length - 1) {
-    mainSection.innerHTML = "";
+    mainSection.innerHTML = ``;
     screenShow(screens[i]);
   } else {
     i = 0;
   }
 };
 
-document.addEventListener("keydown", function(evt) {
+document.addEventListener(`keydown`, function (evt) {
   if (evt.keyCode === RIGHT_KEYCODE) {
     swichRight();
   } else if (evt.keyCode === LEFT_KEYCODE) {
@@ -57,10 +57,10 @@ document.addEventListener("keydown", function(evt) {
   }
 });
 
-RIGHT_BTN.addEventListener("click", function(evt) {
+RIGHT_BTN.addEventListener(`click`, function () {
   swichRight();
 });
 
-LEFT_BTN.addEventListener("click", function(evt) {
+LEFT_BTN.addEventListener(`click`, function () {
   swichLeft();
 });
