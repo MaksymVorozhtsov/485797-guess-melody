f231c390892c6ad9f510f05b78abe7927b1ee880
MaksymVorozhtsov <maxim.vorojtsov@gmail.com>
2018-12-03 17:41:22 +0100

    travis

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,6 @@ const getElementFromTemplate = (element) => {
   let newElement = document.createElement(`div`);
   newElement.innerHTML = element;
   return newElement;
-}
+};
 
 export default getElementFromTemplate;
--- js/main.js
+++ js/main.js
@@ -7,26 +7,26 @@ const mainSection = document.querySelector(`.main`);
 mainSection.appendChild(welcomeElement);
 
 const appendToMain = (element) => {
-  mainSection.innerHTML = "";
+  mainSection.innerHTML = ``;
   mainSection.appendChild(element);
-}
+};
 
 const backButtonFunction = () => {
   const backButton = document.querySelector(`.game__back`);
-  backButton.addEventListener("click", function() {
+  backButton.addEventListener(`click`, function () {
     appendToMain(welcomeElement);
   });
-}
+};
 
 const playButton = document.querySelector(`.welcome__button`);
 
-playButton.addEventListener("click", function() {
+playButton.addEventListener(`click`, function () {
   appendToMain(gameGenreElement);
   backButtonFunction();
 
   const submitButton = document.querySelector(`.game__submit`);
 
-  submitButton.addEventListener("click", function() {
+  submitButton.addEventListener(`click`, function () {
     appendToMain(gameArtistElement);
     backButtonFunction();
   });
