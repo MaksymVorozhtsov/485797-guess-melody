fe636512b6c4361b8c0ebb80b2e1ac143876b932
MaksymVorozhtsov <maxim.vorojtsov@gmail.com>
2018-12-03 17:30:38 +0100

    back btn

--- js/main.js
+++ js/main.js
@@ -6,82 +6,28 @@ const mainSection = document.querySelector(`.main`);
 
 mainSection.appendChild(welcomeElement);
 
+const appendToMain = (element) => {
+  mainSection.innerHTML = "";
+  mainSection.appendChild(element);
+}
+
+const backButtonFunction = () => {
+  const backButton = document.querySelector(`.game__back`);
+  backButton.addEventListener("click", function() {
+    appendToMain(welcomeElement);
+  });
+}
+
 const playButton = document.querySelector(`.welcome__button`);
 
 playButton.addEventListener("click", function() {
-  mainSection.innerHTML = "";
-  mainSection.appendChild(gameGenreElement);
+  appendToMain(gameGenreElement);
+  backButtonFunction();
 
   const submitButton = document.querySelector(`.game__submit`);
 
   submitButton.addEventListener("click", function() {
-    mainSection.innerHTML = "";
-    mainSection.appendChild(gameArtistElement);
+    appendToMain(gameArtistElement);
+    backButtonFunction();
   });
 });
-
-
-
-/*
-const screens = [`welcome`, `game-genre`, `game-artist`, `result-success`, `fail-time`, `fail-tries`, `modal-error`, `modal-confirm`];
-
-const mainSection = document.querySelector(`.main`);
-const arrowsSection = document.querySelector(`.arrows`);
-const arrowsTemplate = document.querySelector(`.arrows__wrap`);
-
-const Enum = {
-  RIGHT_KEYCODE: 39,
-  LEFT_KEYCODE: 37,
-};
-
-arrowsSection.appendChild(arrowsTemplate);
-
-const rightBtn = document.querySelector(`.arrows__btn--right`);
-const leftBtn = document.querySelector(`.arrows__btn--left`);
-
-const screenShow = (id) => {
-  let templateToInsert = document.getElementById(id).content;
-  let newTemplate = templateToInsert.cloneNode(true);
-  mainSection.appendChild(newTemplate);
-};
-
-screenShow(screens[0]);
-
-let i = 0;
-
-const swichRight = () => {
-  i++;
-  if (i > 0 && i <= screens.length - 1) {
-    mainSection.innerHTML = ``;
-    screenShow(screens[i]);
-  } else {
-    i = screens.length - 1;
-  }
-};
-
-const swichLeft = () => {
-  i--;
-  if (i >= 0 && i < screens.length - 1) {
-    mainSection.innerHTML = ``;
-    screenShow(screens[i]);
-  } else {
-    i = 0;
-  }
-};
-
-document.addEventListener(`keydown`, function (evt) {
-  if (evt.keyCode === Enum.RIGHT_KEYCODE) {
-    swichRight();
-  } else if (evt.keyCode === Enum.LEFT_KEYCODE) {
-    swichLeft();
-  }
-});
-
-rightBtn.addEventListener(`click`, function () {
-  swichRight();
-});
-
-leftBtn.addEventListener(`click`, function () {
-  swichLeft();
-});
-*/
